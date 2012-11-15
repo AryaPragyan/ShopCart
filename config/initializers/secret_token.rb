2aa1277e3e9eecb9ad6d92083d2d1227b4b75026
arya <arya@ubuntu.ubuntu-domain>
2012-11-15 13:14:16 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopCart::Application.config.secret_token = '0adc562537205c7735c2161505daa848b3f2c8b7f70a62dff83d54c0cfaa7b444c0fb5aec32d2054a5c028a9b59ccbb1938e8904896280f6274549e8b0f03ae3'
